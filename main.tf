c7a4424108f7b92bae5ee4ff97000745a1427cbb
mj <marusj@op.pl>
2023-07-24 15:25:56 +0200

    Changes

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "private_subnets" {
     for_each          = var.private_subnets
     vpc_id            = aws_vpc.vpc.id
     cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
-    availability_zone = element(data.aws_availability_zones.available.names)
+    availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
     tags = {
         Name = each.key
