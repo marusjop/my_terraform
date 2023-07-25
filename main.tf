7d8484dcb6aff7a0b25af397a4239b812087eb89
mj <marusj@op.pl>
2023-07-25 09:37:56 +0200

    I hashed error things

--- main.tf
+++ main.tf
@@ -14,31 +14,31 @@ resource "aws_vpc" "vpc" {
 }
 
 #Deploy the private subnets
-resource "aws_subnet" "private_subnets" {
-    for_each          = var.private_subnets
-    vpc_id            = aws_vpc.vpc.id
-    cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
-    availability_zone = element(data.aws_availability_zones.available.names, count.index)
+# resource "aws_subnet" "private_subnets" {
+#     for_each          = var.private_subnets
+#     vpc_id            = aws_vpc.vpc.id
+#     cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+#     availability_zone = element(data.aws_availability_zones.available.names, index)
 
-    tags = {
-        Name = each.key
-        Terraform = "true"
-        }
-}
+#     tags = {
+#         Name = each.key
+#         Terraform = "true"
+#         }
+# }
 
 #Deploy the public subnets
-resource "aws_subnet" "public_subnets" {
-    for_each                = var.public_subnets
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
-    availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
-    map_public_ip_on_launch = true
+# resource "aws_subnet" "public_subnets" {
+#     for_each                = var.public_subnets
+#     vpc_id                  = aws_vpc.vpc.id
+#     cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+#     availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
+#     map_public_ip_on_launch = true
 
-    tags = {
-        Name      = each.key
-        Terraform = "true"
-    }
-}
+#     tags = {
+#         Name      = each.key
+#         Terraform = "true"
+#     }
+# }
 
 #Create route tables for public and private subnets
 resource "aws_route_table" "public_route_table" {
@@ -68,19 +68,19 @@ resource "aws_route_table" "private_route_table" {
 
 #Create route table associations
 
-resource "aws_route_table_association" "public" {
-    depends_on     = [aws_subnet.public_subnets]
-    route_table_id = aws_route_table.public_route_table.id
-    for_each       = aws_subnet.public_subnets
-    subnet_id      = each.value.id
-}
+# resource "aws_route_table_association" "public" {
+#     depends_on     = [aws_subnet.public_subnets]
+#     route_table_id = aws_route_table.public_route_table.id
+#     for_each       = aws_subnet.public_subnets
+#     subnet_id      = each.value.id
+# }
 
-resource "aws_route_table_association" "private" {
-    depends_on     = [aws_subnet.private_subnets]
-    route_table_id = aws_route_table.private_route_table.id
-    for_each       = aws_subnet.private_subnets
-    subnet_id      = each.value.id
-    }
+# resource "aws_route_table_association" "private" {
+#     depends_on     = [aws_subnet.private_subnets]
+#     route_table_id = aws_route_table.private_route_table.id
+#     for_each       = aws_subnet.private_subnets
+#     subnet_id      = each.value.id
+#     }
 
 #Create Internet Gateway
 resource "aws_internet_gateway" "internet_gateway" {
@@ -100,13 +100,13 @@ resource "aws_eip" "nat_gateway_eip" {
     }
 }
 
-#Create NAT Gateway
-resource "aws_nat_gateway" "nat_gateway" {
-    depends_on    = [aws_subnet.public_subnets]
-    allocation_id = aws_eip.nat_gateway_eip.id
-    subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+# #Create NAT Gateway
+# resource "aws_nat_gateway" "nat_gateway" {
+#     depends_on    = [aws_subnet.public_subnets]
+#     allocation_id = aws_eip.nat_gateway_eip.id
+#     subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
 
-    tags = {
-        Name = "demo_nat_gateway"
-    }
-}
+#     tags = {
+#         Name = "demo_nat_gateway"
+#     }
+# }
