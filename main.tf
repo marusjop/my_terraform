2d6f4676a81644c6fc6f77dade7886945f086f8c
mj <marusj@op.pl>
2023-07-25 10:14:56 +0200

    I hashed even more

--- main.tf
+++ main.tf
@@ -53,18 +53,18 @@ resource "aws_route_table" "public_route_table" {
         Terraform = "true"
     }
 }
-resource "aws_route_table" "private_route_table" {
-    vpc_id = aws_vpc.vpc.id
-    route {
-        cidr_block= "0.0.0.0/0"
-        # gateway_id = aws_internet_gateway.internet_gateway.id
-        nat_gateway_id = aws_nat_gateway.nat_gateway.id
-        }
-    tags = {
-        Name      = "demo_private_rtb"
-        Terraform = "true"
-        }
-}
+# resource "aws_route_table" "private_route_table" {
+#     vpc_id = aws_vpc.vpc.id
+#     route {
+#         cidr_block= "0.0.0.0/0"
+#         # gateway_id = aws_internet_gateway.internet_gateway.id
+#         nat_gateway_id = aws_nat_gateway.nat_gateway.id
+#         }
+#     tags = {
+#         Name      = "demo_private_rtb"
+#         Terraform = "true"
+#         }
+# }
 
 #Create route table associations
 
