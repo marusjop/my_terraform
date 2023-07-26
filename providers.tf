d617ff61686fb73d2d9b40e77409cae54301fb03
mj <marusj@op.pl>
2023-07-26 12:38:49 +0200

    Dodani kolejni providerzy

--- providers.tf
+++ providers.tf
@@ -6,12 +6,20 @@ terraform {
       version = "~> 3.0"
     }
     http = {
-      source  = "hashicorp/aws"
-      version = "2.1.0"
+      source = "hashicorp/http"
+      #  version = "2.1.0"
     }
     random = {
-      source  = "hashicorp/aws"
-      version = "3.1.0"
+      source = "hashicorp/random"
+      # version = "3.1.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      # version = "2.1.0"      
+    }
+    tls = {
+      source = "hashicorp/tls"
+      # version = "3.1.0"
     }
   }
 }
