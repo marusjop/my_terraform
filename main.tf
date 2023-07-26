812f07653335a8e43a72ec7a20259dd03134a0f1
mj <marusj@op.pl>
2023-07-26 16:32:23 +0200

    Replacing with variables

--- main.tf
+++ main.tf
@@ -186,11 +186,11 @@ resource "random_password" "randomness" {
 
 resource "aws_subnet" "variables-subnet" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.250.0/24"
-  availability_zone       = "eu-north-1a"
-  map_public_ip_on_launch = true
+  cidr_block              = var.variables_sub_cidr
+  availability_zone       = var.variables_sub_az
+  map_public_ip_on_launch = var.variables_sub_auto_ip
   tags = {
-    Name      = "sub-variables-eu-north-1a"
+    Name      = "sub-variables-${var.variables_sub_az}"
     Terraform = "true"
   }
 }
