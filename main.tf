d9a1e17d5e322bd7fabc43f68280c9b98f389978
mj <marusj@op.pl>
2023-07-26 14:57:17 +0200

    Security group added

--- main.tf
+++ main.tf
@@ -161,4 +161,19 @@ resource "aws_s3_bucket" "my-new-S3-bucket" {
   }
 }
 
-
+resource "aws_security_group" "my-new-security-group" {
+  name        = "web_server_inbound"
+  description = "Allow inbound traffic on tcp/443"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    description = "Allow 443 from the Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name    = "web_server_inbound"
+    Purpose = "Intro to Resource Blocks Lab"
+  }
+}
