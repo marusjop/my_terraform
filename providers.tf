d5ee9cb7eb89571fa7fb684beb7f96c5f6e883b5
mj <marusj@op.pl>
2023-07-26 12:28:17 +0200

    Added http and random providers

--- providers.tf
+++ providers.tf
@@ -1,11 +1,21 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#     }
-#   }
-# }
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    http = {
+      source  = "hashicorp/aws"
+      version = "2.1.0"
+    }
+    random = {
+      source  = "hashicorp/aws"
+      version = "3.1.0"
+    }
+  }
+}
 
 provider "aws" {
-  region  = "eu-north-1"
+  region = "eu-north-1"
 }
