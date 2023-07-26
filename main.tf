e52917fbce0a7b9ab066f7cf2be48a3d74176690
mj <marusj@op.pl>
2023-07-26 12:38:59 +0200

    Keys creation

--- main.tf
+++ main.tf
@@ -143,3 +143,11 @@ resource "aws_instance" "web" {
     Name = "Ubuntu EC2 Server 2"
   }
 }
+
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "MyAWSKey.pem"
+}
